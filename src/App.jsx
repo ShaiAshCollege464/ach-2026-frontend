39b3d6f85aaa3662639938e26832d262d1058c70
Shai <shai>
2026-04-28 09:13:36 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,88 @@
+import {useEffect, useState} from 'react'
+import './App.css'
+import axios from "axios";
+
+function App() {
+
+    const [workers, setWorkers] = useState([]);
+    const [selectedWorker, setSelectedWorker] = useState(null);
+    const [teams, setTeams] = useState([]);
+    const [currentTeam, setCurrentTeam] = useState(null)
+
+
+    useEffect(() => {
+        axios.get("http://localhost:8085/get-workers-by-manager").then(resposne => {
+            setWorkers(resposne.data);
+        })
+        axios.get("http://localhost:8085/get-teams").then(resposne => {
+            setTeams(resposne.data);
+        })
+
+    }, []);
+
+    return (
+        <>
+            <div>
+                Worker
+            </div>
+            {
+                workers.map(item => {
+                    return (
+                        <div onClick={() => {
+                            axios.get("http://localhost:8085/get-worker-details?workerId=" + item.id)
+                                .then(response => {
+                                    setSelectedWorker(response.data);
+                                    setCurrentTeam(response.data.teamId);
+                                })
+                        }}>
+                            {item.firstName}
+                        </div>
+                    )
+                })
+            }
+
+            <div>
+                {
+                    selectedWorker &&
+                    <div>
+                        WorkerDetails:
+                        <div>
+                            {selectedWorker.firstName}
+                        </div>
+                        <div>
+                            {selectedWorker.lastName}
+                        </div>
+
+
+                        <div>
+                            Teams:
+
+                            <select value={currentTeam} onChange={(event => {
+                                setCurrentTeam(event.target.value)
+                                axios.get("http://localhost:8085/change-team?workerId=" + selectedWorker.id + "&teamId=" + event.target.value)
+                                    .then(response => {
+                                    })
+                            })}>
+                                {
+                                    teams.map(item => {
+                                        return (
+                                            <option value={item.id}>
+                                                {item.name}
+                                            </option>
+                                        )
+                                    })
+                                }
+
+                            </select>
+
+                        </div>
+
+                    </div>
+                }
+            </div>
+
+        </>
+    )
+}
+
+export default App
